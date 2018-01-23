697752bff36709318dee220cd5a732f861a67f04
Ravi <nararavinara@gmail.com>
2018-01-24 05:10:06 +0530

    sample

---
+++ fotterview.js
@@ -0,0 +1,14 @@
+window.fotterview = Backbone.View.extend({
+
+    initialize: function () {
+        this.render();
+    },
+
+    render: function () {
+        
+         $(this.el).html(this.template());
+        return this;
+        debugger;
+    }
+
+});
---
+++ routs.js
@@ -0,0 +1,20 @@
+var AppRouter =Backbone.Router.extend({
+  routes:{
+    "":'home',
+    "home":'home'
+  },
+  initialize: function () {
+        this.headerView = new HeaderView();
+        $('.header').html(this.headerView.el);
+      this.bodyView = new fotterview();
+        $('.container').html(this.bodyView.el);
+    },
+
+	HeaderView: function(page) {
+    }
+});
+
+utils.loadTemplate(['HeaderView','fotterview'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
